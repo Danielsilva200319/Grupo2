70daa565d1481b102aa2ca8af1812e290b475b43
Daniel silva <89894235+Danielsilva200319@users.noreply.github.com>
2023-10-12 20:59:35 -0500

    Update Departamento.cs

--- core/Entities/Departamento.cs
+++ core/Entities/Departamento.cs
@@ -8,5 +8,6 @@ namespace core.Entities
     public class Departamento : BaseEntity
     {
         public string NombreDep { get; set; }
+        public ICollection<Pais> IdPais { get; set; }
     }
 }
