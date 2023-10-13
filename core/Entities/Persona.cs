6ef7ebe877c8c0253d13871a88d4445095bb6b5e
Daniel silva <89894235+Danielsilva200319@users.noreply.github.com>
2023-10-12 21:07:21 -0500

    Update Persona.cs

--- core/Entities/Persona.cs
+++ core/Entities/Persona.cs
@@ -7,6 +7,10 @@ namespace core.Entities
 {
     public class Persona : BaseEntity
     {
-        
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public ICollection<Genero> IdGenero { get; set; }
+        public ICollection<Ciudad> IdCiudad { get; set; }
+        public ICollection<TipoPersona> IdTipoPersona { get; set; }
     }
 }
